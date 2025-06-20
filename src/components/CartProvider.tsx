a99a78af5cbce9beb27ad51554ce149b28eb7573
0kyon <ozakyon23@gmail.com>
2025-06-20 22:53:50 +0900

    エラー修正

--- src/components/CartProvider.tsx
+++ src/components/CartProvider.tsx
@@ -4,14 +4,14 @@ import { CartProvider as ShoppingCartProvider } from 'use-shopping-cart';
 import { ReactNode } from 'react';
 
 export default function CartProvider({ children }: { children: ReactNode }) {
+  const stripeKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY ?? ''
   return (
     <ShoppingCartProvider
       mode="payment"
       cartMode="client-only"
-      stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string}
+      stripe={stripeKey}
       successUrl={process.env.NEXT_PUBLIC_STRIPE_SUCCESS_URL as string}
       cancelUrl={process.env.NEXT_PUBLIC_STRIPE_CANCEL_URL as string}
-      checkoutSessionUrl={`${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'}/api/checkout`}
       currency="JPY"
       shouldPersist={true}
     >
